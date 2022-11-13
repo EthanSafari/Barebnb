8eec682998c190577d58e6501c5173d1d38e1530
Ethan <eman5797@gmail.com>
2022-11-13 12:31:49 -0500

    signup function,remove user first last name

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,7 +30,20 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
-const initialState = { user: null };
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
 
 export const restoreUser = () => async dispatch => {
   const response = await csrfFetch('/api/session');
@@ -38,6 +51,8 @@ export const restoreUser = () => async dispatch => {
   dispatch(setUser(data.user));
   return response;
 };
+
+const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
   let newState;
