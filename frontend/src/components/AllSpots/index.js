f85072fda3cf2f60c9b631cbde5d160316484f30
Ethan <eman5797@gmail.com>
2022-11-15 17:58:53 -0500

    added all spots to home

--- frontend/src/components/AllSpots/index.js
+++ frontend/src/components/AllSpots/index.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { getAllSpots } from "../../store/spots";
 
 import './AllSpots.css';
@@ -18,23 +19,27 @@ const SpotsList = () => {
 
     const spotsArray = objectToArray(spotsObject);
 
-    console.log('spotsArray:',spotsArray)
+    console.log('spotsArray:', spotsArray)
 
-        useEffect(() => {
-            dispatch(getAllSpots());
-        }, [dispatch]);
+    useEffect(() => {
+        dispatch(getAllSpots());
+    }, [dispatch]);
 
     if (!spotsArray) return null;
     else return (
         <div className="all-spots-container">
             <ul className="all-spots-ul">
                 {spotsArray.map(spot => (
-                    <li key={spot.id} className="all-spots-ul-li">
-                        {!spot.preview ? null
-                        : spot.preview.includes("doesn't have") ? null
-                        : <img className='all-spots-preview-image' src={spot.preview} alt={spot.name}></img>}
-
-                    </li>
+                    <NavLink to={`/spots/${spot.id}`}>
+                        <li key={spot.id} className="all-spots-ul-li">
+                            {!spot.preview ? null
+                                : spot.preview.includes("doesn't have") ? null
+                                    : <img className='all-spots-preview-image' src={spot.preview} alt={spot.name}></img>}
+                            <span>{spot.name}</span>
+                            <span>{spot.price}</span>
+                            <span>{spot.address}</span>
+                        </li>
+                    </NavLink>
                 ))}
             </ul>
         </div>
