b749b1a4c016888ce9c27d7c59fc99cb7a13152b
Ethan <eman5797@gmail.com>
2022-11-20 21:51:27 -0500

    added more css

--- frontend/src/components/SingleSpot/index.js
+++ frontend/src/components/SingleSpot/index.js
@@ -4,7 +4,9 @@ import { deleteSpotById, getAllSpots } from '../../store/spots';
 import UpdateSpotModal from '../UpdateSpot';
 import SpotReviewsById from '../SpotReviews';
 import CreateReviewModal from '../CreateReview';
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+
+import './SingleSpot.css';
 
 const SingleSpot = () => {
     const dispatch = useDispatch();
@@ -34,30 +36,37 @@ const SingleSpot = () => {
     };
 
     return (
-        <div>
+        <div className='full-single-spot-page'>
             <div>
                 {!singleSpot.preview ? null
                     : singleSpot.preview.includes("doesn't have") ? null
-                        : <img className='all-spots-preview-image' src={singleSpot.preview} alt={singleSpot.name}></img>}
-                <h1>{singleSpot.name}</h1>
-                <h3>{singleSpot.description}</h3>
-                <h3>{singleSpot.address}</h3>
-                <h4>{singleSpot.city}, {singleSpot.state}</h4>
-                <h4>{singleSpot.country}</h4>
-                <h5>${singleSpot.price}/night</h5>
-                {sessionUser && sessionUser.id === singleSpot.ownerId ? (
-                    <button onClick={deleteSpot}>
-                        Delete Listing
-                    </button>
-                ) : null}
-                {sessionUser && sessionUser.id === singleSpot.ownerId ? (
-                    <UpdateSpotModal spots={spots} />
-                ) : null}
+                        : <img className='single-spot-image' src={singleSpot.preview} alt={singleSpot.name}></img>}
+                {console.log(singleSpot.SpotImages)}
+                <div>
+                    <h1>{singleSpot.name}</h1>
+                    <h3>{singleSpot.description}</h3>
+                    <h3>{singleSpot.address}</h3>
+                    <h4>{singleSpot.city}, {singleSpot.state}</h4>
+                    <h4>{singleSpot.country}</h4>
+                    <h5>${singleSpot.price}/night</h5>
+                </div>
+                <div>
+                    {sessionUser && sessionUser.id === singleSpot.ownerId ? (
+                        <button onClick={deleteSpot}>
+                            Delete Listing
+                        </button>
+                    ) : null}
+                    {sessionUser && sessionUser.id === singleSpot.ownerId ? (
+                        <UpdateSpotModal spots={spots} />
+                    ) : null}
+                </div>
             </div>
-            <SpotReviewsById spot={singleSpot} />
-            {sessionUser && sessionUser.id === singleSpot.ownerId ?
-                null : <CreateReviewModal spotId={Number(spotId)} />
-            }
+            <div>
+                <SpotReviewsById spot={singleSpot} />
+                {sessionUser && sessionUser.id === singleSpot.ownerId ?
+                    null : <CreateReviewModal spotId={Number(spotId)} />
+                }
+            </div>
         </div>
     );
 };
