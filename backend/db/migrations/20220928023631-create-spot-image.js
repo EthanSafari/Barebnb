0a12cfe1285c3207dbef3cf149f4b735d817140f
Ethan <eman5797@gmail.com>
2022-12-07 14:11:31 -0500

    migrations set up

--- backend/db/migrations/20220928023631-create-spot-image.js
+++ backend/db/migrations/20220928023631-create-spot-image.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+};
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('SpotImages', {
@@ -33,9 +39,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('SpotImages');
+    options.tableName = 'SpotImages';
+    await queryInterface.dropTable(options);
   }
 };
--- backend/db/migrations/20220928023713-create-review-image.js
+++ backend/db/migrations/20220928023713-create-review-image.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+};
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('ReviewImages', {
@@ -29,9 +35,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages');
+    options.tableName = 'ReviewImages';
+    await queryInterface.dropTable(options);
   }
 };
