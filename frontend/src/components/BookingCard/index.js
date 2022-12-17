4abdf6429697b9fe6b220dacbabc8a305b2f259a
Ethan <eman5797@gmail.com>
2022-12-16 21:56:56 -0500

    almost finished with booking card

--- frontend/src/components/BookingCard/index.js
+++ frontend/src/components/BookingCard/index.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './BookingCard.css';
 
 const BookingCard = ({ spot }) => {
+
+    const [submitPushed, setSubmitPushed] = useState(false);
     return (
         <form className="booking-card">
             <div className="price-and-rating">
@@ -8,7 +11,7 @@ const BookingCard = ({ spot }) => {
                     <p><strong>${spot.price}</strong> night</p>
                 </div>
                 <div>
-                    <p><i class="fa-solid fa-star"></i> {spot.avgStarRating.toFixed(1)} ~ {spot.numReviews} {spot.numReviews > 1 ? (<>Reviews</>) : (<>Review</>)}</p>
+                    <p><i class="fa-solid fa-star"></i> {spot.avgStarRating.toFixed(1)} ~ {spot.numReviews} {spot.numReviews === 1 ? (<>Review</>) : (<>Reviews</>)}</p>
                 </div>
             </div>
             <div className='check-dates'>
@@ -37,7 +40,7 @@ const BookingCard = ({ spot }) => {
             <div className='policies'>
                 <div className='non-refundable'>
                     <div className='non-refundable-price'>
-                        Non-refundable ~ ${spot.price * 5} total
+                        Non-refundable ~ ${(spot.price * 5).toFixed()} total
                     </div>
                     <div className='non-refundable-input'>
                         <input type={'radio'} name='policy' />
@@ -45,17 +48,30 @@ const BookingCard = ({ spot }) => {
                 </div>
                 <div className='refundable'>
                     <div className='refundable-price'>
-                        Refundable ~ ${(spot.price * 5) + (spot.price + (spot.price * .75))} total
+                        Refundable ~ ${((spot.price * 5) + (spot.price + (spot.price * .75))).toFixed()} total
                     </div>
                     <div className='refundable-input'>
-                        <input type={'radio'} name='policy' />
+                        <input type={'radio'} name='policy' checked={true}/>
                     </div>
                 </div>
                 <div className='refundable-info'>
-                    Despite this option saying that this is the refundable price to pay, if you forget to cancel within 48hrs of initially creating your booking, you will get nothing back and we keep all your money :)
+                    Despite this option saying that this is the refundable price to pay, if you forget to cancel within 48min of initially creating your booking, you will get nothing back and we keep all your money. Cancel by {new Date().toDateString()} to get a full refund.
                 </div>
             </div>
-
+        <button className='reserve-button' type='button' onClick={e => {
+            e.preventDefault();
+            setSubmitPushed(true);
+        }}>
+            Reserve
+        </button>
+        <div className='charged'>
+            You will be charged once you press the button
+        </div>
+        {submitPushed && (
+            <div className='submit-pushed'>
+                **Feature under construction, will be available soon**
+            </div>
+        )}
         </form>
     )
 };
